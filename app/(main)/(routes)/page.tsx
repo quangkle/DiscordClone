94a38f2efcd6ca073265677f1c8dc9a10770cd9b
quang-khuong-duy-le <nathanle2711@gmail.com>
2023-12-12 22:05:55 -0500

    Add dark light modes and toggle button

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { DarkModeToggle } from "@/components/dark-mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
       <UserButton afterSignOutUrl="/" />
+      <DarkModeToggle />
     
       <p className='text-3xl font-bold text-indigo-500'>
         Hello discord
