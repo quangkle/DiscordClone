200f0c65ed82c9757a21baa39dc6a3ec840affe5
quang-khuong-duy-le <nathanle2711@gmail.com>
2023-12-17 12:40:15 -0500

    Add server sidebar items

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -3,6 +3,7 @@ import { db } from '@/lib/db';
 import { ChannelType } from '@prisma/client';
 import { redirect } from 'next/navigation';
 import React from 'react'
+import ServerHeader from './server-header';
 
 interface ServerSidebarProps {
     serverId: string;
@@ -51,7 +52,10 @@ const ServerSidebar = async ({
 
     return (
         <div className='flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]'>
-            
+            <ServerHeader
+                server={server}
+                role={role}
+            />
         </div>
     )
 }
