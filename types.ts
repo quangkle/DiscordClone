a4f819abad245f7ef5f312f6464869812ddf0063
quang-khuong-duy-le <nathanle2711@gmail.com>
2023-12-27 23:59:27 -0500

    Handle message sending

--- types.ts
+++ types.ts
@@ -1,4 +1,4 @@
-import { Server, Member, Profile } from '@prisma/client'
+import { Server, Member, Profile, Message } from '@prisma/client'
 import { Server as NetServer, Socket } from "net";
 import { NextApiResponse } from 'next';
 import { Server as SocketIOServer } from "socket.io";
@@ -6,6 +6,12 @@ import { Server as SocketIOServer } from "socket.io";
 export type ServerWithMembersWithProfiles = Server & {
     members: (Member & { profile: Profile }) [];
 };
+
+export type MessageWithMemberWithProfile = Message & {
+    member: Member & {
+        profile: Profile
+    }
+}
 
 export type NextApiResponseServerIo = NextApiResponse & {
     socket: Socket & {
